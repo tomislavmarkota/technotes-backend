ce89a958ee5afbe008600f838048291df3f6b62e
unknown <tomislav.markota11@gmail.com>
2022-12-02 13:40:32 +0100

    ready to deploy

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    //'http://localhost:3000',
+    'https://technotes-f9bf.onrender.com',
+    'https://www.technotes-f9bf.onrender.com'
+]
+
+module.exports = allowedOrigins
