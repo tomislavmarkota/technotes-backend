95f933c141b09fa7e9a9503f00946cb366e2df3e
unknown <tomislav.markota11@gmail.com>
2022-12-02 14:32:58 +0100

    second commt

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    //'http://localhost:3000',
-    'https://technotes-f9bf.onrender.com',
-    'https://www.technotes-f9bf.onrender.com'
+    'https://technotes.onrender.com'
 ]
 
 module.exports = allowedOrigins
